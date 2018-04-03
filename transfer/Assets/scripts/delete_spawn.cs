4a769f277b2e136c022e08ddd75f64bef12710f1
Illuminary alpaca (Grant) <36704033+GrantGenesius@users.noreply.github.com>
2018-04-04 02:56:12 +0700

    Added sample scene

---
+++ transfer/Assets/scripts/delete_spawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delete_spawn : MonoBehaviour {
+
+    public float lifetime = 10f;
+
+	void Update () {
+		if(lifetime > 0) {
+            lifetime -= Time.deltaTime;
+            if (lifetime <= 0) {
+                Destruction();
+            }
+        }
+
+        if (this.transform.position.y <= -20) {
+            Destruction();
+        }
+	}
+
+    void OnCollisionEnter(Collision coll) { 
+        if(coll.gameObject.name == "delete_spawn"){
+            Destruction();
+        }
+    }
+
+    void Destruction() {
+        Destroy(this.gameObject);
+    }
+}
---
+++ transfer/Assets/scripts/right_click_ray.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class right_click_ray : MonoBehaviour {
+    public Camera cam;
+    public Transform Spawnpoint;
+    public Rigidbody Prefab;
+
+	void Update () {
+        // right click mouse input
+        //if(Input.GetMouseButtonDown(1)){
+
+        //spacebar input
+        if(Input.GetKeyDown("space")){
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit)){
+                //action here
+                Debug.Log("ray casted using right spacebar");
+
+                //spawn prefab
+                Rigidbody RigidPrefab;
+                RigidPrefab = Instantiate(Prefab, hit.point, Spawnpoint.rotation);
+            }
+        }
+	}
+}
